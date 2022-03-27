19cff6e4f27d68d20701dec89e702ab806a32fe4
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-03-27 21:13:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,48 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int ar1 = 0; ar1 < len - 1; ar1++) {
+        for (int ar2 = ar1 + 1; ar2 < len; ar2++) {
+            if (arr[ar1] + arr[ar2] == value) {
+                count++;
+            } else {
+                continue;
+            }
+        }
+    }
+    cout << count << endl;
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int z = len - 1; z > i; z--) {
+            if (arr[i] + arr[z] == value) {
+                count += 1;
+            }
+        }
+    }
+    cout << count << endl;
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int  j = i + 1;
+        int f = len;
+        while (j < f) {
+            int c = (j + f) / 2;
+            if (arr[c] < (value - arr[i])) {
+                j = c + 1;
+            } else {
+                f = c;
+            }
+        }
+        while (arr[j] + arr[i] == value) {
+            count++;
+            j++;
+        }
+    }
+    cout << count << endl;
+    return count;
 }
